fbfe2dd73c7b7906636677e3ac7f828cfb407757
mssandeepkamath <msandeepcip@gmail.com>
2023-01-18 02:38:54 +0530

    Ui and transaction update

---
+++ app/src/main/java/com/example/tripfactory_concierge_android/activity/DocumentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tripfactory_concierge_android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tripfactory_concierge_android.R
+
+class DocumentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_document)
+    }
+}
--- app/src/main/java/com/example/tripfactory_concierge_android/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/tripfactory_concierge_android/fragment/HomeFragment.kt
@@ -1,19 +1,26 @@
 package com.example.tripfactory_concierge_android.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.example.tripfactory_concierge_android.R
 import com.example.tripfactory_concierge_android.activity.MainActivity
+import com.example.tripfactory_concierge_android.activity.PlaceActivity
+import com.example.tripfactory_concierge_android.activity.RestRoomActivity
+import com.example.tripfactory_concierge_android.activity.RestaurantActivity
 import com.example.tripfactory_concierge_android.databinding.ActivityMainBinding
 import com.example.tripfactory_concierge_android.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(),OnClickListener {
 
 
     private lateinit var bindingHomeFragment: FragmentHomeBinding
@@ -27,20 +34,43 @@ class HomeFragment : Fragment() {
         bindingHomeFragment = FragmentHomeBinding.inflate(inflater, container, false)
         //viewBinding activity_main
         bindingMainActivity=(activity as MainActivity).bindingMainActivity
-        //Open drawer on hamburgerIcon click
-        bindingHomeFragment.ivHamburger.setOnClickListener {
-            bindingMainActivity.lytDrawer.openDrawer(GravityCompat.START)
-        }
         //Close drawer on navigationDrawerHeader click
         bindingMainActivity.vwNavigation.getHeaderView(0).setOnClickListener {
             bindingMainActivity.lytDrawer.closeDrawers()
         }
-        //return rootView
-
+        //Display date
         val formatTime=SimpleDateFormat("hh:mm aa", Locale.US)
         val time=formatTime.format(Date())
         bindingHomeFragment.tvDate.text="${time.substring(0,5)}\n ${time.substring(6)}"
+        //on click declarations
+        bindingHomeFragment.ivHamburger.setOnClickListener(this)
+        bindingHomeFragment.lytResturants.setOnClickListener(this)
+        bindingHomeFragment.lytPlaces.setOnClickListener(this)
+        bindingHomeFragment.lytRestRooms.setOnClickListener(this)
         return bindingHomeFragment.root
+    }
+
+    override fun onClick(v: View?) {
+       when(v?.id)
+       {
+           //Open drawer on hamburgerIcon click
+           R.id.ivHamburger->  bindingMainActivity.lytDrawer.openDrawer(GravityCompat.START)
+           R.id.lytResturants->{
+               intentProvider(RestaurantActivity())
+           }
+           R.id.lytPlaces->{
+               intentProvider(PlaceActivity())
+           }
+           R.id.lytRestRooms->{
+               intentProvider(RestRoomActivity())
+           }
+       }
+    }
+    private fun intentProvider(activityName:Activity)
+    {
+        //Intent to respective activities
+        val intent= Intent(activity as MainActivity,activityName::class.java)
+        (activity as MainActivity).startActivity(intent)
     }
 
 
