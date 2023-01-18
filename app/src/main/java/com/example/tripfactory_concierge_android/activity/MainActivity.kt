f0d3f7e44121fd0ff4a7c78b2945f1ed6d429d45
mssandeepkamath <msandeepcip@gmail.com>
2023-01-18 11:01:20 +0530

    Navigation drawer onClicks added

--- app/src/main/java/com/example/tripfactory_concierge_android/activity/MainActivity.kt
+++ app/src/main/java/com/example/tripfactory_concierge_android/activity/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.tripfactory_concierge_android.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMainActivity.root)
         //Load home fragment on activity create
-        replaceFragment(bindingMainActivity.lytFrame.id,HomeFragment(),"HomeFragment")
+        replaceFragment(bindingMainActivity.lytFrame.id, HomeFragment(), "HomeFragment")
         //check the first item of bottom navigation on start
         bindingMainActivity.vwBottomNavigation.menu.findItem(R.id.home).isChecked = true
 
@@ -26,46 +29,86 @@ class MainActivity : AppCompatActivity() {
         bindingMainActivity.vwBottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
-                  replaceFragment(bindingMainActivity.lytFrame.id,HomeFragment(),"HomeFragment")
+                    replaceFragment(bindingMainActivity.lytFrame.id, HomeFragment(), "HomeFragment")
                 }
                 R.id.chat -> {
-                    replaceFragment(bindingMainActivity.lytFrame.id,SupportChatFragment(),"SupportChatFragment")
+                    replaceFragment(
+                        bindingMainActivity.lytFrame.id,
+                        SupportChatFragment(),
+                        "SupportChatFragment"
+                    )
                 }
                 R.id.storage -> {
-                    replaceFragment(bindingMainActivity.lytFrame.id,StorageFragment(),"StorageFragment")
+                    replaceFragment(
+                        bindingMainActivity.lytFrame.id,
+                        StorageFragment(),
+                        "StorageFragment"
+                    )
                 }
             }
             return@setOnItemSelectedListener true
         }
+
+        //navigation drawer onclick function
+        bindingMainActivity.vwNavigation.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.recommendation -> {
+                    intentProvider(RestaurantActivity())
+                }
+                R.id.support_chat -> {
+                    intentProvider(ChatActivity())
+                }
+                R.id.document -> {
+                    intentProvider(DocumentActivity())
+                }
+                R.id.voucher -> {
+                    intentProvider(VoucherActivity())
+                }
+                R.id.account -> {
+                    intentProvider(AccountActivity())
+                }
+                R.id.rate_us -> {
+                    val playStoreIntent = Intent(Intent.ACTION_VIEW)
+                    intent.data =
+                        Uri.parse("https://play.google.com/store/apps/details?id=com.sandeep.studybear")
+                    bindingMainActivity.lytDrawer.closeDrawers()
+                    startActivity(playStoreIntent)
+                }
+                R.id.privacy -> {
+                    intentProvider(PrivacyPolicyActivity())
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
 
-    private fun replaceFragment(frameLayoutId : Int, fragment:Fragment,tag:String)
-    {
+    private fun replaceFragment(frameLayoutId: Int, fragment: Fragment, tag: String) {
         //replacing the fragment
-        supportFragmentManager.beginTransaction().replace(frameLayoutId,fragment,tag).commit()
+        supportFragmentManager.beginTransaction().replace(frameLayoutId, fragment, tag).commit()
     }
+
 
     override fun onBackPressed() {
         //get the fragment currently attached to frame layout
-        val fragment=supportFragmentManager.findFragmentById(bindingMainActivity.lytFrame.id)
-        when(fragment)
-        {
+        val fragment = supportFragmentManager.findFragmentById(bindingMainActivity.lytFrame.id)
+        when (fragment) {
             //if fragment attached is not home fragment, then on back pressed come back to home fragment
-            !is HomeFragment->
-            {
-                replaceFragment(bindingMainActivity.lytFrame.id,HomeFragment(),"HomeFragment")
+            !is HomeFragment -> {
+                replaceFragment(bindingMainActivity.lytFrame.id, HomeFragment(), "HomeFragment")
                 bindingMainActivity.vwBottomNavigation.menu.findItem(R.id.home).isChecked = true
             }
             //Else follow the default behaviour of activity stack pop
-            else->super.onBackPressed()
+            else -> super.onBackPressed()
         }
     }
 
 
-
-
-
-
+    private fun intentProvider(activityName: Activity) {
+        //Intent to respective activities
+        val intent = Intent(this, activityName::class.java)
+        bindingMainActivity.lytDrawer.closeDrawers()
+        startActivity(intent)
+    }
 
 
 }
